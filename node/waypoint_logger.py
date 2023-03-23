acf2f52fb4a5ac4be2567ed4ad7d607e5cf9b535
pe712 <61479003+pe712@users.noreply.github.com>
2023-03-23 17:19:22 +0100

    Update waypoint_logger.py

--- node/waypoint_logger.py
+++ node/waypoint_logger.py
@@ -2,23 +2,15 @@
 import rospy
 import numpy as np
 from os.path import dirname
-from os import rename
 from tf.transformations import euler_from_quaternion
 from nav_msgs.msg import Odometry
 import matplotlib.pyplot as plt
 from sys import argv
 from re import search
 
-from tf2_ros import Buffer, TransformListener
-from tf2_geometry_msgs import do_transform_pose
-
-
 # ___________________________________ Lecture de l'image PGM ___________________________________
 def plot_fig(map_pathname):
-    image = np.zeros((0,0))
-    width, height = 0,0
     # show the map img
-    print("reading map file...")
     with open(map_pathname, 'rb') as f:
         buffer = f.read()
         match = search(
@@ -28,43 +20,15 @@ def plot_fig(map_pathname):
             b"(\d+)\s(?:\s*#.*[\r\n]\s)*)", buffer)
         if match:
             header, width, height, maxval = match.groups()
-            width, height, maxval = int(width), int(height), int(maxval)
-            image = np.frombuffer(buffer,
-                            dtype='u1' if maxval < 256 else '<u2',
-                            count=width*height,
+        else:
+            raise ValueError("Not a raw PGM file: '%s'" % map_pathname)
+        image = np.frombuffer(buffer,
+                            dtype='u1' if int(maxval) < 256 else '<u2',
+                            count=int(width)*int(height),
                             offset=len(header)
-                            ).reshape((height, width))
-    if not match:
-        # it can be a P2 (ascii file)
-        with open(map_pathname, 'r') as f:
-            lines = f.readlines()
-            # Ignores commented lines
-            count=-1
-            for l in lines:
-                if l[0] == '#':
-                    lines.remove(l)
-                    count+=1
-            # Makes sure it is ASCII format (P2)
-            assert lines[0].strip() == 'P2' 
-            sz = lines[1].split()
-            width, height = int(sz[0]), int(sz[1])
-            # Converts data to a list of integers
-            image = np.zeros((height, width))
-            x,y = 0,0
-            for line in lines[2:]:
-                for value in line.split():
-                    image[y,x]=value
-                    x+=1
-                    if x==width:
-                        y+=1
-                        x=0
-                    if y==height:
-                        print("wrong dimensions")
-                        break
-    image=np.transpose(image)
+                            ).reshape((int(height), int(width)))
     plt.imshow(image, cmap='gray', origin='lower')
     # get parameters of the img
-    print("plotting waypoints...")
     yaml_file_pathname = map_pathname.replace(".pgm", ".yaml")
     with open(yaml_file_pathname, 'r') as f:
         head = f.readline()
@@ -72,28 +36,23 @@ def plot_fig(map_pathname):
         origin = f.readline().split(": ")[1].replace("[", "").split(", ")
         origin = [float(origin[0]), float(origin[1])]
     # Lecture des coordonnees
-    coords=np.array((0, 2))
-    try:
-        coords = np.loadtxt("/home/pe/catkin_ws/src/f1tenth_simulator/fichiers_csv/waypoints.csv", delimiter=",", usecols=(0, 1))
-    except IOError:
-        # file do not exist yet
-        pass
+    data = np.loadtxt("/home/pe/catkin_ws/src/f1tenth_simulator/fichiers_csv/waypoints.csv", delimiter=",")
+    coords = data[:, :2]
     # Transformation des coordonnees
     scale = 1/resolution
     for i, (dx, dy) in enumerate(coords):
-        newy = scale * (-origin[0] + dx)
-        newx = -scale * (-origin[1] + dy)+height
-        plt.scatter(newx, newy, color='r')
-        plt.annotate(str(i), (newx, newy))
+        newx = scale * (origin[0] + dx)
+        newy = scale * (origin[1] + dy)
+        coords[i]=(newx, newy)
+    for i, (x, y) in enumerate(coords):
+        plt.scatter(x, y, color='r')
+        plt.annotate(str(i), (x,y))
+        print(x,y)
     plt.savefig(node_directory+"/../fichiers_csv/waypoints.png")
     plt.show()
 
 # ___________________________________ Enregistrement du fichier csv apres avoir parcouru le circuit avec la voiture ___________________________________
 def save_waypoint(data):
-    # transform = tfBuffer.lookup_transform("odom", "map", data.header.stamp)
-    # pose_transformed = do_transform_pose(data.pose, transform)
-    # print(pose_transformed.pose.position.x, data.pose.pose.position.x)
-    # print("looping...")
     quaternion = np.array([data.pose.pose.orientation.x, 
                            data.pose.pose.orientation.y, 
                            data.pose.pose.orientation.z, 
@@ -103,28 +62,24 @@ def save_waypoint(data):
     speed = (data.twist.twist.linear.x**2 +data.twist.twist.linear.y**2+data.twist.twist.linear.z**2)**0.5
     if data.twist.twist.linear.x>0.:
         print(data.twist.twist.linear.x)
-    
-    if (float(data.pose.pose.position.x)!=0 or float(data.pose.pose.position.y)!=0):
-        file.write('%f, %f, %f, %f\n' % (data.pose.pose.position.x,
-                                        data.pose.pose.position.y,
-                                        euler[2],
-                                        speed))
+
+    file.write('%f, %f, %f, %f\n' % (data.pose.pose.position.x,
+                                     data.pose.pose.position.y,
+                                     euler[2],
+                                     speed))
 
 def listener():
-    # rospy.Subscriber('/odom', Odometry, save_waypoint)
+    rospy.init_node('waypoints_logger', anonymous=True)
     rospy.Subscriber('pf/pose/odom', Odometry, save_waypoint)
     rospy.spin()
 
 # ___________________________________ Conserver uniquement les waypoints utiles ___________________________________
 def tronque_boucle_csv(start, end):
-    print("truncating...")
     with open(node_directory+'/../fichiers_csv/waypoints.csv', 'r') as f:
         lines = f.readlines()
         with open(node_directory+'/../fichiers_csv/truncated_waypoints.csv', 'w') as new_f:
             for line in lines[start:end]:
                 new_f.write(line)
-    rename(node_directory+'/../fichiers_csv/waypoints.csv', node_directory+'/../fichiers_csv/old_waypoints.csv')
-    rename(node_directory+'/../fichiers_csv/truncated_waypoints.csv', node_directory+'/../fichiers_csv/waypoints.csv')
 
 
 if __name__ == '__main__':
@@ -136,9 +91,6 @@ if __name__ == '__main__':
         args =argv[1].split("-")
         if args[0]=="record":
             print('Saving waypoints...')
-            rospy.init_node('waypoints_logger', anonymous=True)
-            # tfBuffer = Buffer()
-            # tf_listener = TransformListener(tfBuffer) 
             with open(node_directory+'/../fichiers_csv/waypoints.csv', 'w') as file:
                 listener()
         elif args[0]=="show":
